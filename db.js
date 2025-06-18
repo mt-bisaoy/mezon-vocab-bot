2c1b65dd3726d2153a77c4971f379e28a98a9914
mt-bisaoy <mchi8894@gmail.com>
2025-06-18 17:08:23 +0700

    fix: add env for db connection

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
     const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false, // Quan trọng nếu dùng Railway cloud
+  },
 });
 
 const query = (text, params) => pool.query(text, params);
